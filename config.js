71ab58980d416ef637c59439681fe92fb11212ed
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-06-17 14:10:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //=============『 Utama 』================== //
-global.owner = ['6282138919347'] 
+global.owner = ['6283822205585'] 
 global.mods = [] 
 global.prems = [] 
 
@@ -19,8 +19,8 @@ global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig,
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/8921f83300b1238e90510.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/ddde6d6417d18e67a4c89.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'Neslite Max'
@@ -29,15 +29,15 @@ global.wm2 = '                     「 Neslite Max あ⁩ 」'
 global.wm3 = '⫹⫺ Neslite Max'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/8921f83300b1238e90510.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
-global.benar = 'Benar ✅\n'
-global.salah = 'Salah ❌\n'
+global.benar = 'Benar \n'
+global.salah = 'Salah \n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Helga Zexs'
+global.author = '© Neslite Max'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
